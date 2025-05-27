01375dfd0e54c727691d585017a16c681eabc615
naveen-b26 <naveenbandari370@gmail.com>
2025-05-27 11:32:31 +0530

    added the product details in amazon.js

---
+++ scripts/amazon.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+        rating: {
+            stars: 4.5,
+            count: 87
+        },
+        price: 10.90
+    },
+    {
+        name: "Intermediate Size Basketball",
+        image: 'images/products/intermediate-composite-basketball.jpg',
+        rating: {
+            stars: 4.0,
+            count: 127
+        },
+        price: 20.95
+    },
+    {
+        name: "Adults Plain Cotton T-Shirt - 2 Pack",
+        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        rating: {
+            stars: 4.5,
+            count: 56
+        },
+        price: 7.99
+
+    }
+];
